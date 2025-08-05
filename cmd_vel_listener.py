ecd577be4fa4a2835366d2afb3d392856b0e39b2
Mihaila Gabriel-Codrut (uih38915) <gabriel-codrut.mihaila@continental-corporation.com>
2025-08-05 15:32:46 +0300

    Adaugare tot

--- cmd_vel_listener.py
+++ cmd_vel_listener.py
@@ -63,14 +63,21 @@ def generate_launch_description():
             'map_subscribe_transient_local', default_value='true',
             description='Whether to set the map subscriber QoS to transient local'),
 
-        # RPLIDAR node
+    
         Node(
             package='rplidar_ros',
             executable='rplidar_composition',
-            name='rplidar_node',
-            parameters=[{'serial_port': '/dev/ttyUSB0', 'frame_id': 'laser_frame', 'use_sim_time': use_sim_time}],
-            output='screen'
-        ),
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+                'frame_id': 'laser_frame',
+                'angle_compensate': True,
+                'scan_mode': 'Standard',
+                'use_sim_time': use_sim_time
+
+            }],
+            name='rplidar_node'
+        )
 
         # Existing nodes
         Node(
